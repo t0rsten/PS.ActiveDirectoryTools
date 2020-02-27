6c5f171a817a0e1e041e4d56c8ae20a09900cb54
Torsten <46536955+t0rsten@users.noreply.github.com>
2020-02-27 07:12:03 +0100

    Create Get-tADFSMOHolders.ps1

---
+++ public/Get-tADFSMOHolders.ps1
@@ -0,0 +1,38 @@
+﻿function Get-tADFSMOHolders{
+  <#
+  .Synopsis
+    Get FSMO Holders.
+
+  .DESCRIPTION
+    Get FSMO holders in forest and current domain.
+
+  .EXAMPLE
+    Get-tADFSMOHolders
+    Get FSMO holders.
+
+  .NOTES
+    Author: Torsten Demmich
+    Mail:   torsten.demmich@gmail.com
+    Version:	1.0.0
+
+    --- ChangeLog ---
+    - Version 1.0.0
+      - Initial Version
+    
+  #>
+    [CmdletBinding()]
+    Param()
+
+    $Domain = Get-ADDomain
+    $Forest = Get-ADForest
+
+    $FSMOHolders = New-Object PSObject -Property @{
+      DomainNaming = $Forest.DomainNamingMaster
+      Schema = $Forest.SchemaMaster
+      Infrastructure = $Domain.InfrastructureMaster
+      PDC = $Domain.PDCEmulator
+      RID = $Domain.RIDMaster
+    }
+
+    Return $FSMOHolders
+  }
