40f8c9f9c645832a64074625a2ba33db2377f0cc
Torsten <46536955+t0rsten@users.noreply.github.com>
2020-02-28 16:40:26 +0100

    Create

---
+++ public/Get-tADStaleAccounts.ps1
@@ -0,0 +1,50 @@
+﻿function Get-tADStaleAccounts{
+<#
+.Synopsis
+  Get stale accounts.
+
+.DESCRIPTION
+  Get stale Active Directory accounts.
+
+.PARAMETER SearchBase
+  Specifies the OU to search under.
+
+.EXAMPLE
+  Get-tADStaleAccounts
+  Get stale accounts in the current domain.
+
+.EXAMPLE
+  Get-tADStaleAccounts -SearchBase 'OU=Sales,DC=expamle,DC=com'
+  Get stale accounts in OU Sales.
+
+.NOTES
+  Author: Torsten Demmich
+  Mail:   torsten.demmich@gmail.com
+  Version:	1.0.0
+
+  --- ChangeLog ---
+  Version 1.0.0
+    - Initial Version
+#>
+  [CmdletBinding()]
+  Param(
+    [Parameter(Mandatory=$false)]
+    [String]$SearchBase = (Get-ADDomain).DistinguishedName,
+    [Parameter(Mandatory=$false)]
+    $Days = '30'
+  )
+
+  begin {
+    Write-Verbose -Message ('SearchBase: {0}' -f $SearchBase)
+    Write-Verbose -Message ('Days: {0}' -f $Days)
+  }
+
+  process {
+    $Result = Search-ADAccount -AccountInactive -TimeSpan $Days -SearchBase $SearchBase | Select-Object SID, Name, objectClass
+  }
+
+  end {
+    Return $Result
+  }
+
+}
